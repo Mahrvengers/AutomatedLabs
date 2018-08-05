dafe0faaa31329282f818106fbf7ef1bd58a7634
Stefan Hoffmann <Stefan.Hoffmann@mahr-edv.de>
2018-08-05 14:35:04 +0200

    Heutige Mitschrift hinzugefügt!

---
+++ 001-Erstes-Test-System.ps1
@@ -0,0 +1,21 @@
+﻿<#
+
+	Ein einfaches erstes Testsystem zu Demonstrationszwecken!
+
+#>
+
+
+$labName = "GettingStarted"
+
+New-LabDefinition -Name $labName -DefaultVirtualizationEngine HyperV
+
+# Make the network definition
+Add-LabVirtualNetworkDefinition -Name $labName -HyperVProperties @{
+	SwitchType = 'External';
+	AdapterName = 'WLAN'}
+
+Add-LabMachineDefinition -Name DC01 -OperatingSystem 'Windows Server 2016 Standard (Desktop Experience)' -Network $labName -Memory 4GB -Processors 2
+
+Install-Lab
+
+Show-LabDeploymentSummary
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AutomatedLabs
 Unsere automatisierten Lab-Skripte für Demos und Übungen
+
+Unsere Lab-Skripte bekommen durch diese Software Sinn und Leben: 
+
+https://github.com/AutomatedLab/AutomatedLab
+
+Folge den Anweisungen oder lass dir von Iron Cloud, Torch oder SQL Server Junge helfen, wenn du Herausforderungen haben solltest.
+
